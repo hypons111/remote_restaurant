97d20811d2981abec7a554d92ec82ecff4b28ee8
Hypons <hypons@me.com>
2020-06-18 13:24:36 +0800

    added sorting function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,10 +8,46 @@ const Todo = require('../../models/todo')
 router.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(restaurant => res.render('index', { restaurant }))
     .catch(error => console.error(error))
 })
 
+//  首頁 asc
+router.get('/asc', (req, res) => {
+  Todo.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+//  首頁 desc
+router.get('/desc', (req, res) => {
+  Todo.find()
+    .lean()
+    .sort({ _id: 'desc' })
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+//  首頁 category
+router.get('/category', (req, res) => {
+  Todo.find()
+    .lean()
+    .sort({ category: 'asc' })
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+//  首頁 location
+router.get('/location', (req, res) => {
+  Todo.find()
+    .lean()
+    .sort({ location: 'asc' })
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.error(error))
+})
 
 // 匯出路由模組
 module.exports = router
